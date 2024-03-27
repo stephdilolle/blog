f34ba1cdfe5d355fe972dbd52b66c70f5597918e
sdilolle <stephdilolle@gmail.com>
2024-03-27 17:52:18 -0400

    initial changes to html, css, and js for main page

---
+++ assets/js/form.js
@@ -0,0 +1,8 @@
+const button = document.getElementById('button');
+const usernameInput = document.getElementById('username');
+const titleInput = document.getElementById('title');
+const contentInput = document.getElementById('content');
+
+button.addEventListener('click', function(event) {
+    event.preventDefault();
+});
